de8af8c9ff22f7de2440554dc88ac0a1d4bc0798
Vikas-700 <kvikas5705@gmail.com>
2024-02-17 08:28:39 +0530

    Some Changes of 17feb2024

--- first.js
+++ first.js
@@ -1,6 +1,13 @@
-alert("Wellcome in my website.")
-console.log("Hello gyes!")
-fullName="Vikas kumar"
-console.log(fullName)
-age=19
-console.log(age)
+alert("Wellcome in my website.");
+console.log("Hello gyes!");
+fullName="Vikas kumar";
+console.log(fullName);
+age=19;
+console.log(age);
+var video = document.getElementById("song");
+
+        video.addEventListener("ended", function() {
+            video.currentTime = 0; // Resetting the currentTime to restart playback
+            video.play(); // Start playing again
+        }
+        );
