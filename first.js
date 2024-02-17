077fedfe062dca019f438accb52185bbc7ccf671
Vikas-700 <kvikas5705@gmail.com>
2024-02-17 13:09:12 +0530

    Add new

--- first.js
+++ first.js
@@ -4,10 +4,3 @@ fullName="Vikas kumar";
 console.log(fullName);
 age=19;
 console.log(age);
-var video = document.getElementById("song");
-
-        video.addEventListener("ended", function() {
-            video.currentTime = 0; // Resetting the currentTime to restart playback
-            video.play(); // Start playing again
-        }
-        );
